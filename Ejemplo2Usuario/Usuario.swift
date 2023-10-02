26b5982eda5ac1a1f5e3c55deda5dc74367bfdd2
carloscatalinacastello <carlos.catalina.castello@cieep.com>
2023-10-02 16:38:24 +0200

    ejemplo finalizado

---
+++ Ejemplo2Usuario/Usuario.swift
@@ -0,0 +1,13 @@
+//
+//  Usuario.swift
+//  Ejemplo2Usuario
+//
+//  Created by Carlos Catalina Castelló on 02/10/2023.
+//
+
+import Foundation
+
+struct Usuario {
+    var email: String
+    var password: String
+}
--- Ejemplo2Usuario/ViewController.swift
+++ Ejemplo2Usuario/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import Toast
 
 class ViewController: UIViewController {
-
+   //VARIABLES DE VISTA
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    //VARIABLES LÓGICAS
+    var listaUsuarios:[Usuario]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        listaUsuarios = []
     }
-
-
+    
+    @IBAction func registrarUsuarios(_ sender: Any) {
+        let email: String = txtEmail.text!
+        let password: String = txtPassword.text!
+        
+        let usuario: Usuario = Usuario(email: email, password: password)
+        
+        listaUsuarios.append(usuario)
+        
+        self.view.makeToast("número de usuarios: \(listaUsuarios.count)")
+        
+        txtEmail.text=""
+        txtPassword.text=""
+    }
+    
+    
+    
 }
 
